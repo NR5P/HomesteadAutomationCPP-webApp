decdd9379bec934c28943fd049c4450f5090efa0
NR5P <orangev8zcar@gmail.com>
2019-07-27 21:50:22 -0500

    started working on views and just started working on database

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const path = require("path");
+const mongoose = require("mongoose");
+
 const app = express();
 
-//static folders
-app.use(express.static(path.join(__dirname, "web")));
+/***************handlebars middleware***************************************/
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+/****************end middleware**********************************************/
+
+app.use(express.static(path.join(__dirname, "web"))); // static folders
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/settings", (req, res) => {
+    res.render("settings");
+})
 
 const PORT = process.env.PORT || 5000; // check port number environment variable first
     
