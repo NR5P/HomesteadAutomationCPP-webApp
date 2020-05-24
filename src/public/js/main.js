0a930489680ca7249ea3ad442e7e18bdcca8fd18
NR5P <orangev8zcar@gmail.com>
2020-05-24 14:05:17 -0500

    added days to irrigate to irrigation create

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,41 +5,42 @@ let arrDevices = []
 
 // build the buttons on main page from db
 window.onload = () => {
-    fetch("/api/irrigationDevices", {method: "GET"})
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(function(item) {
-                arrDevices.push(new IrrigationDevice(
-                    item.id,item.name,item.pin,item.notes, item.state, item.cycleOnTimeArray, item.startTimesArray)
-                )
+    if (document.getElementById("main-area") !== null) {
+        fetch("/api/irrigationDevices", {method: "GET"})
+            .then(res => res.json())
+            .then(data => {
+                data.forEach(function(item) {
+                    arrDevices.push(new IrrigationDevice(
+                        item.id,item.name,item.pin,item.notes, item.state, item.cycleOnTimeArray, item.startTimesArray)
+                    )
+                })
             })
-        })
-    fetch("/api/cycleIrrigationDevices", {method: "GET"})
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            data.forEach(function(item) {
-                arrDevices.push(new CycleIrrigationDevice(
-                    item.id, item.name, item.pin, item.notes, item.state, item.cycleOnTimeHr, item.cycleOnTimeMin, 
-                    item.cycleOnTimeSec, item.cycleOffTimeHr, item.cycleOffTimeMin, item.cycleOffTimeSec, 
-                    item.blackoutStartTime, item.blackoutStopTime
-                ))
+        fetch("/api/cycleIrrigationDevices", {method: "GET"})
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                data.forEach(function(item) {
+                    arrDevices.push(new CycleIrrigationDevice(
+                        item.id, item.name, item.pin, item.notes, item.state, item.cycleOnTimeHr, item.cycleOnTimeMin, 
+                        item.cycleOnTimeSec, item.cycleOffTimeHr, item.cycleOffTimeMin, item.cycleOffTimeSec, 
+                        item.blackoutStartTime, item.blackoutStopTime
+                    ))
+                })
             })
-        })
 
-    const addDeviceBtn = document.getElementById("drop-btn");
-    const dropdownContent = document.getElementById("dropdown-content");
-    if (addDeviceBtn !== null) {
-        addDeviceBtn.addEventListener("click", () => {
-            if (dropdownContent.classList.contains("show")) {
-                dropdownContent.classList.remove("show");
-                dropdownContent.classList.add("noshow");
-            } else {
-                dropdownContent.classList.add("show");
-                dropdownContent.classList.remove("noshow");
-            }
-        })
+        const addDeviceBtn = document.getElementById("drop-btn");
+        const dropdownContent = document.getElementById("dropdown-content");
+        if (addDeviceBtn !== null) {
+            addDeviceBtn.addEventListener("click", () => {
+                if (dropdownContent.classList.contains("show")) {
+                    dropdownContent.classList.remove("show");
+                    dropdownContent.classList.add("noshow");
+                } else {
+                    dropdownContent.classList.add("show");
+                    dropdownContent.classList.remove("noshow");
+                }
+            })
+        }
     }
-
 }
 
